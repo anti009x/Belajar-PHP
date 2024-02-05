33daa516e852b8ef199cbb20c7b336557f716b14
anti009x <riskisuleman76@gmail.com>
2024-02-05 14:49:09 +0700

    Fix Update Login dan Register + Exception Handler Register Duplicate

--- Layout/register.php
+++ Layout/register.php
@@ -1,3 +1,47 @@
+    <?php
+
+
+include"../Service/database.php";
+
+
+
+     $database_name = "phplearn";
+     mysqli_select_db($conn , $database_name);
+
+     if (!$conn){
+        echo "Error";
+     };
+
+
+
+     try {
+        if (isset($_POST["register"])){
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+    
+            
+    
+            $sql = "INSERT INTO users (username,password) VALUES  ('$username','$password')";
+    
+            if($conn->query($sql)){
+    
+                echo "Success";
+        
+            }else{
+                echo "Data Ridak Terkirim";
+            }
+        }
+
+
+
+      } catch (Exception ) {
+        echo "Error Brooo Akun Sudah Terdaftar";
+      }
+      mysqli_close($conn);
+
+   
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,10 +51,10 @@
 </head>
 <body>
     <h1>Halaman Register</h1>
-    <form>
+    <form action="register.php" method="POST">
         <input type="text" placeholder="username" name="username"/>
         <input type="password" placeholder="password" name="password"/>
-        <button type="submit">Daftar</button>
+        <button type="submit" name="register">Daftar</button>
     </form>
 </body>
 </html>
